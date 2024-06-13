c6babe780a90890d034542d4b8ae4841f8a7e8fc
achi30 <archie.enukidze@gmail.com>
2024-06-13 23:03:36 +0300

    commit

---
+++ level 07/task1.py
@@ -0,0 +1,8 @@
+my_name="achi"
+lastname="enukidze"
+age="13"
+country="georgia"
+myfavorite_car ="porsche"
+myfavorite_color ="blue"
+myfavorite_number ="7"
+print(my_name,lastname,age,country,myfavorite_car,myfavorite_color,myfavorite_number)
---
+++ level 07/task3.py
@@ -0,0 +1,4 @@
+print("salami rogor xar?")
+print("kargad shen?")
+print("mec kargad dges garet gamoxval kaladburti vitamashot")
+print("kargi,5ze amoval stadionze")
